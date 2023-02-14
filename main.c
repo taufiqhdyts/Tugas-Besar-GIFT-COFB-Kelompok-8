10b04e2995910af90dbc118f4304934aa68ec102
Daffa Muhammad Rasyid <13221060@std.stei.itb.ac.id>
2023-02-14 14:30:02 +0700

    Tubes Kel 8 Belom Selesai

--- main.c
+++ main.c
@@ -1,1 +1,49 @@
-lalalala
+#include <stdio.h>
+#include <string.h>
+
+#define BLOCK_SIZE 16
+#define KEY_SIZE 16
+
+// GIFT block cipher function
+void GIFT_encrypt(unsigned char *block, unsigned char *key) {
+    // ... implement the GIFT encryption function ...
+}
+
+// GIFT-COFB encryption function
+void GIFT_COFB_encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *ciphertext) {
+    int i, j;
+    unsigned char block[BLOCK_SIZE], iv[BLOCK_SIZE];
+
+    // initialize the IV
+    memset(iv, 0, BLOCK_SIZE);
+
+    // encryption loop
+    for (i = 0; i < plaintext_len; i += BLOCK_SIZE) {
+        // XOR the plaintext block with the previous ciphertext block
+        for (j = 0; j < BLOCK_SIZE; j++) {
+            block[j] = plaintext[i + j] ^ iv[j];
+        }
+
+        // encrypt the block with the GIFT block cipher
+        GIFT_encrypt(block, key);
+
+        // store the ciphertext
+        memcpy(ciphertext + i, block, BLOCK_SIZE);
+
+        // update the IV
+        memcpy(iv, block, BLOCK_SIZE);
+    }
+}
+
+int main() {
+    unsigned char plaintext[] = "plaintext message";
+    int plaintext_len = strlen((char *)plaintext);
+    unsigned char key[] = "key for encryption";
+    unsigned char ciphertext[plaintext_len];
+
+    GIFT_COFB_encrypt(plaintext, plaintext_len, key, ciphertext);
+
+    printf("Ciphertext: %s\n", ciphertext);
+
+    return 0;
+}
